9d346e1ceb0eb0599c317adb76207718206f7707
John Duarte <john@yeliad.us>
2015-08-10 14:34:32 -0700

    Add static source for windows install

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,5 +4,14 @@ class mumble::client (
   $package_manage  = $mumble::params::client_package_manage,
   $package_name    = $mumble::params::client_package_name,
 ) inherits mumble::params {
+
+  case $::operatingsystem {
+    'Windows': {
+      $package_source = 'https://github.com/mumble-voip/mumble/releases/download/1.2.10/mumble-1.2.10.msi'
+    }
+    default: {
+    }
+  }
+
   contain '::mumble::client::install'
 }
--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -3,10 +3,11 @@ class mumble::client::install {
 
   if $mumble::client::package_manage {
 
-    package { 'mumble_client':
+    package { 'mumble-client':
       ensure          => $mumble::client::package_ensure,
       install_options => $mumble::client::install_options,
       name            => $mumble::client::package_name,
+      source          => $mumble::client::package_source,
     }
 
   }
