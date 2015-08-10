54134193ce0e1fdf9035f6ce9fee055fd098b189
John Duarte <john@yeliad.us>
2015-08-10 14:58:02 -0700

    Switch to open3 for function

--- lib/puppet/parser/functions/get_latest.rb
+++ lib/puppet/parser/functions/get_latest.rb
@@ -1,7 +1,8 @@
 module Puppet::Parser::Functions
   newfunction(:get_latest, :type => :rvalue) do |args|
+    require 'open3'
     cmd = 'curl -L https://github.com/mumble-voip/mumble/releases/latest | sed -n "s/.*href=\"\(.*\)\.msi\".*/\1/p"'
-    latest = exec cmd
+    latest = Open3.capture2(cmd)[0].chomp
     unless latest.empty? then
       return 'https://github.com' + latest
     else
