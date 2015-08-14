8ee34be0792fecacd402de6013f11a92f592a65d
John Duarte <john@yeliad.us>
2015-08-14 09:51:57 -0700

    Add error trapping to latest release function

--- lib/puppet/parser/functions/get_latest_mumble_release_url_base.rb
+++ lib/puppet/parser/functions/get_latest_mumble_release_url_base.rb
@@ -3,15 +3,26 @@ require 'open-uri'
 module Puppet::Parser::Functions
   newfunction(:get_latest_mumble_release_url_base, :type => :rvalue) do |args|
     suffix = args[0]
-    html = open('https://github.com/mumble-voip/mumble/releases/latest').read
+    begin
+      html = open('https://github.com/mumble-voip/mumble/releases/latest').read
+      rescue SocketError
+      html = ''
+    end
     res = /(?<latest>[\/\w\.-]+)\.msi/.match(html)
+    if res == nil then
+      return ''
+    end
     latest = res[:latest]
     latest = 'https://github.com' + latest + '.' + suffix
-    sig = open("#{latest}.sig").read
+    begin
+      sig = open("#{latest}.sig").read
+      rescue SocketError
+      sig = ''
+    end
     if /BEGIN PGP SIGNATURE/.match(sig) then
-      latest
+      return latest
     else
-      ''
+      return ''
     end
   end
 end
